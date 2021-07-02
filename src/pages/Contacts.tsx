6ba13fd9c87de16a4727178f8c769b83fe50a7e1
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2021-07-02 10:38:59 +0400

    are work

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 export const Contacts = () => {
     return <> 
     
-    <h1>Контакты </h1>
-
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
-    <div>+79268924515</div>
+    <div className="content">
+        <div className="container container--cart">
+             <div className="cart cart--empty">
+            <img src="https://millmarket.ru/img/contacts.137a0f2d.jpeg"  alt="Контакты" />
+            <h3>Контакты:</h3>
+            <br/>
+            <h4>Фактический адрес: 111219, Москва, Калужское шоссе, 22-й километр, дом 10, строение 23.</h4>
+            <br/>
+            <h4>Электронная почта: millmarket.ru@gmail.com.</h4>  
+            <br/> 
+            <h4>Телефоны: +7 495 023-02-43.</h4>
+            <h3>Реквизиты:</h3>
+            <br/>
+            <h4>ИП Магомеддибиров Магомеддибир Курбаналиевич.</h4>
+            <h4>ИНН 051602812148 / ОГРНИП 320057100049777.</h4>
+            <br/>
+            <Link to="/" className="button button--black">
+              <span>Вернуться назад</span>
+            </Link>
+          </div> 
+        </div>
+      </div>
 
    
     </>
