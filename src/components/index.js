634a4af5e13306d7dd93690d80465bfe9a288486
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2021-07-03 17:05:57 +0400

    kk

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import { Header } from './Header';
-import {Categories} from './Categories'
-import {Button} from './Button'
-import {SortPopup} from './SortPopup'
-
-
-export {Header, Categories, Button, SortPopup};
